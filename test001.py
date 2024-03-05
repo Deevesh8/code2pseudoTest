ff292387857dfa8234b40424483c5305b25ae41f
Deevesh8 <152176036+Deevesh8@users.noreply.github.com>
2024-03-05 15:01:59 +0400

    Update test001.py

--- test001.py
+++ test001.py
@@ -1,10 +1,7 @@
-# Unicode string to encode
 unicode_text = "Hello, world! 🌎"
 
-# Encode Unicode string to UTF-8 bytes
 utf8_bytes = codecs.encode(unicode_text, 'utf-8')
 print("Encoded UTF-8 bytes:", utf8_bytes)
 
-# Decode UTF-8 bytes back to Unicode string
 decoded_text = codecs.decode(utf8_bytes, 'utf-8')
 print("Decoded Unicode text:", decoded_text)
